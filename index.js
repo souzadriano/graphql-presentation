698ec1a4407f891467c4702bf3e4f071e76179c1
Adriano de Souza <souzadriano@gmail.com>
2019-04-28 13:11:10 -0300

    change default port

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(helmet());
 
 app.use('', express.static('public'));
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
 });
